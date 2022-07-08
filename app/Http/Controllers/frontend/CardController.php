7609084f71417297b95460463a850a86f35025f7
Pelcos <patryk8.pe@gmail.com>
2022-07-08 17:33:28 +0200

    #6_dodanie_do_koszyka
    #6_dodanie_do_koszyka

---
+++ app/Http/Controllers/frontend/CardController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cart;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+
+class CardController extends Controller
+{
+    public function index()
+    {
+        return view('frontend.cart', [
+                   'categoriesList' => Category::with('children')->whereNull('parent_id')->get()
+            ]
+        );
+    }
+
+    public function addProduct(Product $product)
+    {
+        $card = Cart::create(['user_id' => auth()->id(), 'product_id' => $product->id]);
+    }
+
+    public function showProducts()
+    {
+        $card = Cart::with('products')->where('user_id', auth()->id())->get();
+
+        return response()->json($card);
+    }
+
+    public function deleteProduct(Request $request)
+    {
+        dd($request);
+    }
+}
---
+++ resources/js/AddProductComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <button class="btn btn-success" @click.prevent="addProduct()">Dodaj Produkt</button>
+</template>
+
+<script>
+export default {
+    props: ['product'],
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        addProduct() {
+            axios.post('/produkt/' + this.product)
+                .then((response) => {
+                    this.$toast.success(`Produkt został dodany!`);
+                })
+                .catch((error) => {
+                    this.errors = error.response.data.errors;
+                })
+        },
+    }
+}
+</script>
+<style scoped>
+
+</style>
